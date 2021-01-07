d163726d97e6061c3794ee149ba4346095d007b1
Mynor Meza <mynormeza92@gmail.com>
2021-01-07 08:12:40 -0600

    Initial commit

---
+++ app/src/main/java/com/mynormeza/sampletodo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mynormeza.sampletodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.nav_host_fragment)
+        toolbar =  findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+}
---
+++ app/src/main/java/com/mynormeza/sampletodo/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.mynormeza.sampletodo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
